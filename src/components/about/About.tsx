23530be1ef9d43667b2481f34133d1bb991fc1c6
Eric Lezotte <elezotte@gmail.com>
2023-02-20 07:47:29 -0700

    add custom map layer controls to fix issue when moving map

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,19 +1,9 @@
-import { Button } from '@mui/material'
+import { Button, ButtonGroup } from '@mui/material'
 import Box from '@mui/material/Box'
 import { commonStyles } from 'components/common.styles'
 import GoogleMapReact, { Maps } from 'google-map-react'
 import { useState } from 'react'
 import { styles } from './about.styles'
-
-const lat = parseFloat(process.env.GOOGLE_MAPS_LAT as string)
-const lng = parseFloat(process.env.GOOGLE_MAPS_LON as string)
-const mapProps = {
-  center: {
-    lat,
-    lng,
-  },
-  zoom: 15,
-}
 
 interface mapLatLong {
   lat: number
@@ -25,7 +15,7 @@ interface mapBounds {
   sw: mapLatLong
   ne: mapLatLong
 }
-interface onMapChangeProps {
+interface MapChangeProps {
   center: {
     lat: number
     lng: number
@@ -38,6 +28,28 @@ interface onMapChangeProps {
     height: number
   }
 }
+enum MapTypes {
+  ROADMAP = 'roadmap',
+  SATELLITE = 'satellite',
+  HYBRID = 'hybrid',
+  TERRAIN = 'terrain',
+}
+
+const MapTypeText = {
+  [MapTypes.ROADMAP]: 'Road',
+  [MapTypes.SATELLITE]: 'Satellite',
+  [MapTypes.HYBRID]: 'Hybrid',
+  [MapTypes.TERRAIN]: 'Terrain',
+}
+const lat = parseFloat(process.env.GOOGLE_MAPS_LAT as string)
+const lng = parseFloat(process.env.GOOGLE_MAPS_LON as string)
+const mapProps = {
+  center: {
+    lat,
+    lng,
+  },
+  zoom: 15,
+}
 
 const renderMarkers = (map: any, maps: any) =>
   new maps.Marker({
@@ -45,14 +57,24 @@ const renderMarkers = (map: any, maps: any) =>
     map,
   })
 const handleOnLoad = (gmap: any) => renderMarkers(gmap.map, gmap.maps)
+const getTypeStyles = (selectedType: MapTypes, buttonType: MapTypes) => {
+  return selectedType === buttonType
+    ? styles.typeButtonSelected
+    : styles.typeButton
+}
 
 export default function About() {
   const [center, setCenter] = useState(mapProps.center)
   const [zoom, setZoom] = useState(mapProps.zoom)
+  const [type, setType] = useState(MapTypes.TERRAIN)
 
-  const handleOnChange = (props: onMapChangeProps) => {
+  const handleOnChange = (props: MapChangeProps) => {
     setZoom(props.zoom)
     setCenter(props.center)
+  }
+  const handleTypeChange = (newType: MapTypes) => {
+    console.log('newType', newType)
+    setType(newType)
   }
   const handleMapReset = () => {
     setCenter({
@@ -60,6 +82,7 @@ export default function About() {
       lng: mapProps.center.lng,
     })
     setZoom(mapProps.zoom)
+    setType(MapTypes.TERRAIN)
   }
 
   return (
@@ -80,8 +103,8 @@ export default function About() {
           onChange={handleOnChange}
           options={(maps: Maps) => ({
             fullscreenControl: false,
-            mapTypeControl: true,
-            mapTypeId: maps.MapTypeId.TERRAIN,
+            mapTypeControl: false,
+            mapTypeId: type,
             mapTypeControlOptions: {
               style: maps.MapTypeControlStyle.DROPDOWN_MENU,
               position: maps.ControlPosition.TOP_LEFT,
@@ -99,8 +122,21 @@ export default function About() {
           sx={styles.resetButton}
           variant="contained"
         >
-          Reset View
+          Reset
         </Button>
+        <ButtonGroup sx={styles.typeButtonGroup} orientation="vertical">
+          {Object.entries(MapTypes).map((buttonType) => (
+            <Button
+              color="secondary"
+              onClick={() => handleTypeChange(buttonType[1])}
+              key={buttonType[1]}
+              sx={getTypeStyles(type, buttonType[1])}
+              variant="contained"
+            >
+              {MapTypeText[buttonType[1]]}
+            </Button>
+          ))}
+        </ButtonGroup>
       </Box>
     </>
   )
--- src/components/about/about.styles.ts
+++ src/components/about/about.styles.ts
@@ -1,4 +1,18 @@
-import { colors, MuiSxStyles } from 'components/theme/themeSettings'
+import { SxProps } from '@mui/material'
+import { colors, font, MuiSxStyles } from 'components/theme/themeSettings'
+
+const googleButtonStyles: SxProps = {
+  backgroundColor: colors.background,
+  color: '#666',
+  fontWeight: 500,
+  fontSize: font.size.MD,
+  textTransform: 'none',
+
+  '&:hover': {
+    color: colors.gray.dark,
+    backgroundColor: colors.background,
+  },
+}
 
 export const styles: MuiSxStyles = {
   map: {
@@ -10,18 +24,25 @@ export const styles: MuiSxStyles = {
     transform: 'translate(-50%, -100%)',
     color: colors.warning.medium,
   },
+  typeButtonGroup: {
+    position: 'absolute',
+    top: 10,
+    left: 10,
+  },
+  typeButton: googleButtonStyles,
+  typeButtonSelected: {
+    ...googleButtonStyles,
+    backgroundColor: '#ddd',
+    color: '#777',
+    '&:hover': {
+      backgroundColor: '#ddd',
+      color: '#777',
+    },
+  },
   resetButton: {
     position: 'absolute',
     top: 10,
     right: 10,
-    backgroundColor: colors.background,
-    color: '#666',
-    fontWeight: 700,
-    textTransform: 'none',
-
-    '&:hover': {
-      color: colors.gray.dark,
-      backgroundColor: colors.background,
-    },
+    ...googleButtonStyles,
   },
 }
