974ce732db9c06462a8ff99ddd39e81dc951fe8c
Eric Lezotte <elezotte@gmail.com>
2024-02-06 20:34:13 -0700

    add latest meeting minutes

--- src/components/information/informationItems.meetings.ts
+++ src/components/information/informationItems.meetings.ts
@@ -2,15 +2,21 @@ import { ListIconTypes, ListItem } from 'components/common/itemList/ItemList'
 
 export const meetingItems: ListItem[] = [
   {
-    id: 'meeting-minutes-02/25/2023',
-    label: 'Meeting Minutes - 2/25/2023',
-    path: '/documents/meeting-minutes/2_25_2023-ley_subdivision-meeting_minutes.pdf',
+    id: 'meeting-minutes-02/03/2024',
+    label: 'Meeting Minutes - 2/3/2024',
+    path: '/documents/meeting-minutes/2_3_2024-ley_subdivision-meeting_minutes.pdf',
     iconType: ListIconTypes.DOCUMENT,
   },
   {
     id: 'meeting-minutes-08/05/2023',
     label: 'Meeting Minutes - 8/5/2023',
     path: '/documents/meeting-minutes/8_5_2023-ley_subdivision-meeting_minutes.pdf',
+    iconType: ListIconTypes.DOCUMENT,
+  },
+  {
+    id: 'meeting-minutes-02/25/2023',
+    label: 'Meeting Minutes - 2/25/2023',
+    path: '/documents/meeting-minutes/2_25_2023-ley_subdivision-meeting_minutes.pdf',
     iconType: ListIconTypes.DOCUMENT,
   },
 ]
