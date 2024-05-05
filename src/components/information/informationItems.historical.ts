02ce40a04eaa9410f729a3f9b385c9d107f719c9
Eric Lezotte <elezotte@gmail.com>
2024-05-05 15:33:50 -0600

    fix historal photos link

--- src/components/information/informationItems.historical.ts
+++ src/components/information/informationItems.historical.ts
@@ -2,16 +2,16 @@ import { ListIconTypes, ListItem } from 'components/common/itemList/ItemList'
 
 export const historicalItems: ListItem[] = [
   {
-    id: 'conquistadore-ski-area',
-    label: 'Conquistador Ski Area',
-    path: 'http://www.coloradoskihistory.com/lost/conquistador.html',
+    id: 'historical-photos',
+    label: 'Historical Photos of Westcliffe',
+    path: 'https://digital.denverlibrary.org/nodes/search?datefrom=&dateto=&keywords=westcliffe&order=desc&orderby=relevance&ntids=W10=&page=1&viewtype=grid&type=&in=0&digital=0',
     iconType: ListIconTypes.WEBSITE,
     rel: 'external',
   },
   {
-    id: 'historical-photos',
-    label: 'Historical Photos of Westcliffe',
-    path: 'https://digital.denverlibrary.org/digital/collection/p15330coll22/search/searchterm/westcliffe%2C%20colorado',
+    id: 'conquistadore-ski-area',
+    label: 'Conquistador Ski Area',
+    path: 'http://www.coloradoskihistory.com/lost/conquistador.html',
     iconType: ListIconTypes.WEBSITE,
     rel: 'external',
   },
